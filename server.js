3844e390662e1d8320035db3f769cd2b1f2337b0
Lexius Waltar <lexius.waltar@gmail.com>
2021-11-08 12:18:21 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const port = 3050;
 
 // app.use("/html-time", express.static("public"));
-app.use(express.static("src"));
 app.use(express.static('dist'));
 
 //set the server to listen at the port
